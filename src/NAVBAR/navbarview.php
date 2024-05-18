135dcc95a777771973b77c1876f3e93562ac75eb
LabiadWeb <labiadmo920@gmail.com>
2024-05-18 18:39:05 +0100

    labiad

--- src/NAVBAR/navbarview.php
+++ src/NAVBAR/navbarview.php
@@ -19,31 +19,34 @@
         <div class="absolute inset-0 bg-blue-950"></div>
 
         <div class="relative px-4 mx-auto sm:px-6 lg:px-8">
-            <nav class="flex items-center justify-between h-16 lg:h-20">
+            <nav class="flex flex-initial  justify-between items-center  h-16 lg:h-20">
                 <div class="flex-shrink-0">
                     <a href="#" title="" class="flex">
                         <img style="height: 180px;" class="w-auto h-8 lg:h-10" src="../images/logo.png" alt="" />
                     </a>
                 </div>
 
-                <button type="button" class="inline-flex p-2 text-white transition-all duration-200 rounded-md lg:hidden focus:bg-gray-800 hover:bg-gray-800">
+                <button type="button" class="inline-flex  p-2 text-white transition-all duration-200 rounded-md lg:hidden focus:bg-gray-800 hover:bg-gray-800">
                     <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
                     </svg>
                 </button>
 
                 <div class="hidden lg:flex lg:items-center lg:space-x-10">
-                    <a href="#" title="" class="text-base font-medium text-white"> Products </a>
+                    <a href="#" title="" class="text-base font-medium text-white"> Home </a>
                     
 
-                    <a href="#" title="" class="text-base font-medium text-white"> About </a>
+                    <a href="#" title="" class="text-base font-medium text-white"> Products</a>
 
                     <a href="#" title="" class="text-base font-medium text-white"> About </a>
 
-                    <a href="#" title="" class="text-base font-medium text-white"> Pricing </a>
+                    <a href="#" title="" class="text-base font-medium text-white"> Contact Us </a>
+                    
+
                 </div>
-
+                <a href="" class=" text-white"><i  class="fa-solid fa-cart-shopping"></i></a>
                 <a href="#" title="" class="items-center justify-center hidden px-6 py-3 text-base font-semibold text-black transition-all duration-200 bg-yellow-400 border border-transparent rounded-full lg:inline-flex hover:bg-yellow-500 focus:bg-yellow-500" role="button">Login</a>
+                
             </nav>
         </div>
     </div>
@@ -65,6 +68,7 @@
                 <a href="#" title="" class="flex w-full py-2 font-medium text-white transition-all duration-200 focus:text-opacity-70"> Resources </a>
 
                 <a href="#" title="" class="flex w-full py-2 font-medium text-white transition-all duration-200 focus:text-opacity-70"> Pricing </a>
+                
             </nav>
 
             <div class="flex flex-col items-start">
@@ -75,6 +79,6 @@
 </header>
 
   
-  
+<script src="https://kit.fontawesome.com/beb14da2c9.js" crossorigin="anonymous"></script>
 </body>
 </html>
