9b4b1dafac1160fc0d5f8f68801a6fb48e40f443
LabiadWeb <labiadmo920@gmail.com>
2024-05-19 17:29:35 +0100

    labiad

--- src/HOME/homeview.php
+++ src/HOME/homeview.php
@@ -9,8 +9,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-        <p class = "text-black" >HOME</p>
+<style>
+    *{
+        color:#333333 ;
+    }
+    @font-face{
+    font-family:"helvetica";
+    src:url("https://candyfonts.com/wp-data/2018/10/26/11538/HELR45W.ttf") format("woff"),
+    url("https://candyfonts.com/wp-data/2018/10/26/11538/HELR45W.ttf") format("opentype"),
+    url("https://candyfonts.com/wp-data/2018/10/26/11538/HELR45W.ttf") format("truetype");
+}
+</style>
+<body >
+        <?php include '../Carousel.php'?>   
+        
+        <div class="flex m-10 justify-between">
+        
+        <span class="shrink-0 px-6 font-[helvetica] font-bold">Deals of the Month : </span>
+        <a href="http://" class ="  " ><button class=" rounded-full bg-black w-28 h-9 font-[helvetica] text-white">View All <i class="fa-solid fa-arrow-right text-white "> </i></button></a>
+        
+        
+        </div>
+        
+        
+</span>
+
 </body>
 </html>
 <?php 
--- src/NAVBAR/navbarview.php
+++ src/NAVBAR/navbarview.php
@@ -14,37 +14,44 @@
 }
 
 </style>
+<style>
+    .circle {
+        width: 16px;
+        height: 16px;
+        border-radius: 50%; /* This makes the element circular */
+        background-color: #3DB2FF ; /* Adjust color as needed */
+    }
+</style>
 <body class=" font-[helvetica]">
-        <nav style = "background-color :#F4FAFF ; " class="xs:opacity-0 sm:opacity-0  md:opacity-100 pl-5 pr-5 p-2  md:flex md:items-center jus md:flex md:justify-between" >
-            <p class ="pl-5" >Due to the COVID 19 epidemic, orders may be processed with a slight delay</p>
-            <div class="pr-5 "><a href="" class="mr-5 hover:text-cyan-500 duration-500">Login / Register</a><a href="" class=" hover:text-cyan-500 duration-500"><i class="mr-5 fa-solid fa-heart"></i>Favories </a></div>
+        <nav style = " background-color :#F4FAFF ; " class="font-[helvetica] hidden  pl-10 pr-5 p-2  md:items-center jus md:flex md:justify-between" >
+            <p class ="pl-5"  >Due to the COVID 19 epidemic, orders may be processed with a slight delay</p>
+            <div class="pr-5 "><a href="" class="mr-5 hover:text-cyan-500 duration-500"><i class="fa-regular fa-user"></i> Login / Register</a><a href="" class=" hover:text-cyan-500 duration-500"><i class="mr-5 fa-solid fa-heart"></i>Favories </a></div>
             
         </nav>
-        <nav class="pl-5 pr-5 p-2 bg-white  md:flex md:items-center  " >
+        <nav class="ml-8 pl-5 pr-5 p-2 bg-white  md:flex md:items-center  justify-between" >
+            
+            <div><img src="../images/LogoPharma.png" class="h-16 inline mr-10" ></img></div>
 
-            <div><img src="../images/LogoPharma.png" class="h-16 inline md:ml-5" ></img></div>
-            <ul class = " md:flex md:ml-56" style="font-size:1.2rem;">
-                <li class="mx-5">
+
+            <ul class = " md:flex " style="font-size:1.08rem;">
+                <li class="mx-8">
                     <a href="#" class=" text-black hover:text-cyan-500 duration-500 " >Home</a>
                 </li>
-                <li class="mx-5">
+                <li class="mx-8">
                     <a href="#" class=" text-black hover:text-cyan-500 duration-500 " >Produits</a>
                 </li>
-                <li class="mx-5">
+                <li class="mx-8">
                     <a href="#" class=" text-black hover:text-cyan-500 duration-500 " >About Us</a>
                 </li>
-                <li class="mx-5">
-                    <a href="#" class=" text-black hover:text-cyan-500 duration-500 " >Contacter</a>
-                </li>
-                <li class="md:ml-80 ml-5">
-                    <a href="#" class=" text-black hover:text-cyan-500 duration-500 " ><i style="font-size:1.4rem; margin-top: 7px;"class=" fa-solid fa-cart-shopping"></i></a>
-                </li>
-                <li class="mx-5 ">
-                  
+                <li class="mx-8">
+                    <a href="#" class=" text-black hover:text-cyan-500 duration-500 " >Contact Us</a>
                 </li>
             </ul>
-
+            
+            <a href="#" class=" mr-10 ml-10 text-black hover:text-cyan-500 duration-500 flex " ><i style="font-size:1.4rem; margin-top: 7px;"class=" fa-solid fa-cart-shopping"></i><div style="margin-left:-7px; font-size:0.9rem;" class="text-white flex justify-center items-center  border circle"><p class ="font-[helvetica]">0</p></div></a>
+            
         </nav>
+        
 
 <script src="https://kit.fontawesome.com/beb14da2c9.js" crossorigin="anonymous"></script>
 </body>
