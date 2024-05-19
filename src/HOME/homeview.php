3af8d12e407b7e245fce200564edd10d414754be
LabiadWeb <labiadmo920@gmail.com>
2024-05-19 02:34:52 +0100

    labiad

---
+++ src/HOME/homeview.php
@@ -0,0 +1,18 @@
+<?php 
+    include '../NAVBAR/navbarview.php'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <p class = "text-black" >HOME</p>
+</body>
+</html>
+<?php 
+    include '../Footer/Footerview.php'
+?>
--- src/NAVBAR/navbarview.php
+++ src/NAVBAR/navbarview.php
@@ -5,80 +5,47 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="../output.css" rel="stylesheet">
 </head>
-<body>
-  <header>
+<style type="text/css">
+@font-face{
+    font-family:"helvetica";
+    src:url("https://candyfonts.com/wp-data/2018/10/26/11538/HELR45W.ttf") format("woff"),
+    url("https://candyfonts.com/wp-data/2018/10/26/11538/HELR45W.ttf") format("opentype"),
+    url("https://candyfonts.com/wp-data/2018/10/26/11538/HELR45W.ttf") format("truetype");
+}
 
-    
+</style>
+<body class=" font-[helvetica]">
+        <nav style = "background-color :#F4FAFF ; " class="xs:opacity-0 sm:opacity-0  md:opacity-100 pl-5 pr-5 p-2  md:flex md:items-center jus md:flex md:justify-between" >
+            <p class ="pl-5" >Due to the COVID 19 epidemic, orders may be processed with a slight delay</p>
+            <div class="pr-5 "><a href="" class="mr-5 hover:text-cyan-500 duration-500">Login / Register</a><a href="" class=" hover:text-cyan-500 duration-500"><i class="mr-5 fa-solid fa-heart"></i>Favories </a></div>
+            
+        </nav>
+        <nav class="pl-5 pr-5 p-2 bg-white  md:flex md:items-center  " >
 
-    </div>
-    <!-- lg+ -->
-    <div class="relative bg-b">
-        <div class="absolute inset-0">
-        </div>
+            <div><img src="../images/LogoPharma.png" class="h-16 inline md:ml-5" ></img></div>
+            <ul class = " md:flex md:ml-56" style="font-size:1.2rem;">
+                <li class="mx-5">
+                    <a href="#" class=" text-black hover:text-cyan-500 duration-500 " >Home</a>
+                </li>
+                <li class="mx-5">
+                    <a href="#" class=" text-black hover:text-cyan-500 duration-500 " >Produits</a>
+                </li>
+                <li class="mx-5">
+                    <a href="#" class=" text-black hover:text-cyan-500 duration-500 " >About Us</a>
+                </li>
+                <li class="mx-5">
+                    <a href="#" class=" text-black hover:text-cyan-500 duration-500 " >Contacter</a>
+                </li>
+                <li class="md:ml-80 ml-5">
+                    <a href="#" class=" text-black hover:text-cyan-500 duration-500 " ><i style="font-size:1.4rem; margin-top: 7px;"class=" fa-solid fa-cart-shopping"></i></a>
+                </li>
+                <li class="mx-5 ">
+                  
+                </li>
+            </ul>
 
-        <div class="absolute inset-0 bg-blue-950"></div>
+        </nav>
 
-        <div class="relative px-4 mx-auto sm:px-6 lg:px-8">
-            <nav class="flex flex-initial  justify-between items-center  h-16 lg:h-20">
-                <div class="flex-shrink-0">
-                    <a href="#" title="" class="flex">
-                        <img style="height: 180px;" class="w-auto h-8 lg:h-10" src="../images/logo.png" alt="" />
-                    </a>
-                </div>
-
-                <button type="button" class="inline-flex  p-2 text-white transition-all duration-200 rounded-md lg:hidden focus:bg-gray-800 hover:bg-gray-800">
-                    <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
-                    </svg>
-                </button>
-
-                <div class="hidden lg:flex lg:items-center lg:space-x-10">
-                    <a href="#" title="" class="text-base font-medium text-white"> Home </a>
-                    
-
-                    <a href="#" title="" class="text-base font-medium text-white"> Products</a>
-
-                    <a href="#" title="" class="text-base font-medium text-white"> About </a>
-
-                    <a href="#" title="" class="text-base font-medium text-white"> Contact Us </a>
-                    
-
-                </div>
-                <a href="" class=" text-white"><i  class="fa-solid fa-cart-shopping"></i></a>
-                <a href="#" title="" class="items-center justify-center hidden px-6 py-3 text-base font-semibold text-black transition-all duration-200 bg-yellow-400 border border-transparent rounded-full lg:inline-flex hover:bg-yellow-500 focus:bg-yellow-500" role="button">Login</a>
-                
-            </nav>
-        </div>
-    </div>
-
-    <!-- xs to lg -->
-    <nav class="flex flex-col justify-between w-full max-w-xs min-h-screen px-4 py-10 bg-black sm:px-6 lg:hidden">
-        <button type="button" class="inline-flex p-2 text-white transition-all duration-200 rounded-md focus:bg-gray-800 hover:bg-gray-800">
-            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-        </button>
-
-        <div class="flex flex-col flex-grow h-full">
-            <nav class="flex flex-col flex-1 mt-10 space-y-2">
-                <a href="#" title="" class="flex w-full py-2 font-medium text-white transition-all duration-200 focus:text-opacity-70"> Features </a>
-
-                <a href="#" title="" class="flex w-full py-2 font-medium text-white transition-all duration-200 focus:text-opacity-70"> Solutions </a>
-
-                <a href="#" title="" class="flex w-full py-2 font-medium text-white transition-all duration-200 focus:text-opacity-70"> Resources </a>
-
-                <a href="#" title="" class="flex w-full py-2 font-medium text-white transition-all duration-200 focus:text-opacity-70"> Pricing </a>
-                
-            </nav>
-
-            <div class="flex flex-col items-start">
-                <a href="#" title="" class="inline-flex items-center justify-center w-auto px-6 py-3 mt-auto text-base font-semibold text-black transition-all duration-200 bg-yellow-400 border border-transparent rounded-full hover:bg-yellow-500 focus:bg-yellow-500" role="button"> Join Now </a>
-            </div>
-        </div>
-    </nav>
-</header>
-
-  
 <script src="https://kit.fontawesome.com/beb14da2c9.js" crossorigin="anonymous"></script>
 </body>
 </html>
