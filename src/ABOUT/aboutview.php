a37bb5a140438a63066586e3a9236911260bd94f
LabiadWeb <labiadmo920@gmail.com>
2024-05-23 18:44:44 +0100

    labid

--- src/ABOUT/aboutview.php
+++ src/ABOUT/aboutview.php
@@ -7,9 +7,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../output.css" rel="stylesheet">
     <title>Document</title>
 </head>
-<body>
+<style>
+
+    *{
+  font-family: "Poppins", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+}
+</style>
+<body style="background-color:white " class="poppins">
         <p class = "text-black font-bold ml-5 mb-4 pt-4" >ABOUT US SECTION</p>
         <p class="ml-12 text-gray-600 text-base pr-64 mt-7">Mais je dois vous expliquer comment cette idée erronée de dénoncer le plaisir et de louer la douleur est née, et je vous donnerai un compte rendu complet du système, en exposant les véritables enseignements du grand explorateur de la vérité, le maître bâtisseur de l'humanité. Tout cela, juste ici, sur Pharma.</p>
         <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
