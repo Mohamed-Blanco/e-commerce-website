a97e82f5a2ff830a69de5f039391a2e2cd2b3320
Bdiro23 <zirarbe@gmail.com>
2024-05-23 17:45:20 +0100

    rf

---
+++ src/ABOUT/aboutview.php
@@ -0,0 +1,104 @@
+<?php 
+    include '../NAVBAR/navbarview.php'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <p class = "text-black font-bold ml-5 mb-4 pt-4" >ABOUT US SECTION</p>
+        <p class="ml-12 text-gray-600 text-base pr-64 mt-7">Mais je dois vous expliquer comment cette idée erronée de dénoncer le plaisir et de louer la douleur est née, et je vous donnerai un compte rendu complet du système, en exposant les véritables enseignements du grand explorateur de la vérité, le maître bâtisseur de l'humanité. Tout cela, juste ici, sur Pharma.</p>
+        <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-8 text-center pt-14">
+        <div class="bg-white shadow-lg rounded-lg p-8">
+            <h3 class="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r  from-blue-500 to-teal-400">
+                100K+
+            </h3>
+            <p class="mt-2 text-xl font-semibold text-gray-900">Monthly visits</p>
+            
+        </div>
+        <div class="bg-white shadow-lg rounded-lg p-8">
+            <h3 class="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
+                12K+
+            </h3>
+            <p class="mt-2 text-xl font-semibold text-gray-900">Customers</p>
+           
+        </div>
+        <div class="bg-white shadow-lg rounded-lg p-8">
+            <h3 class="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r  from-blue-500 to-teal-400">
+                20+
+            </h3>
+            <p class="mt-2 text-xl font-semibold text-gray-900">Industry Awards</p>
+        </div>
+    </div>
+</div>
+<section class="py-10 bg-gray-100 sm:py-16 lg:py-24">
+    <div class="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+        <div class="grid items-center grid-cols-1 gap-y-8 lg:grid-cols-2 gap-x-16 xl:gap-x-24">
+            <div class="relative mb-12">
+                <img class="w-full rounded-md" src="https://enovathemes.com/propharm/wp-content/uploads/post9.jpg" alt="" />
+            </div>
+
+            <div>
+                <h4 class="text-2xl font-bold leading-tight text-black sm:text-2xl lg:text-2xl lg:leading-tight">Ce qui nous définit</h4>
+                <p class="mt-6 text-base leading-relaxed text-gray-600">Nous remettons en question les idées reçues sur le plaisir et la douleur dans la santé. Guidés par des pionniers de la recherche médicale, nous éclairons des vérités cachées pour ouvrir de nouvelles voies vers la guérison.</p>
+                <h4 class="mt-2 text-2xl font-bold leading-tight text-black sm:text-2xl lg:text-2xl lg:leading-tight">Nos valeurs</h4>
+                <p class="mt-6 text-base leading-relaxed text-gray-600">Intégrité : Nos actions et recherches sont menées avec le plus grand respect des normes éthiques.
+           Innovation : À la pointe de la recherche, nous développons des solutions améliorant durablement la santé.
+        Engagement : Notre dévouement pour la santé et le bien-être est profond et inébranlable.</p>
+            </div>
+        </div>
+    </div>
+</section>
+<section class=" bg-gray-50 pb-3.5">
+    <div class="max-w-4xl px-4 mx-auto sm:px-6 lg:px-8">
+        <div class="grid items-center grid-cols-1 gap-y-6 md:grid-cols-2 md:gap-x-20">
+            <div class="py-6">
+                <h5 class="text-2xl font-bold leading-tight text-black sm:text-2xl lg:text-2xl lg:leading-tight">Pourquoi nous choisir ?</h5>
+                <p class="mt-4 text-base leading-relaxed text-gray-600">Nous défions les idées traditionnelles sur le plaisir et la douleur, en vous apportant les véritables enseignements des grands pionniers. Nous cherchons à utiliser le plaisir de manière rationnelle pour éviter les conséquences négatives.</p>
+                <h5 class="text-2xl font-bold leading-tight text-black sm:text-2xl lg:text-2xl lg:leading-tight">Technologie que nous utilisons</h5>
+                <p class="mt-4 text-base leading-relaxed text-gray-600">Notre technologie avancée n'est pas destinée à supporter la douleur mais à dépasser les méthodes conventionnelles pour apporter des innovations significatives.</p>
+            </div>
+                    <img class="rounded-md w-full h-full py-4" src="https://enovathemes.com/propharm/wp-content/uploads/post6.jpg" alt=""/>
+        </div>
+    </div>
+</section>
+<section class="py-10 bg-white sm:py-16 lg:py-24">
+    <h6 class="text-2xl text-center font-bold text-blue-800">Follow us in instagram @pharma</h6>
+    <div class="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+        <div class="flex gap-2 mt-8">
+            <div class="flex justify-center w-full sm:w-1/2 md:w-1/3 lg:w-1/6"> 
+                <img class="rounded-md w-60 h-40" src="https://enovathemes.com/propharm/wp-content/uploads/post6.jpg" alt="Description de l'image" />
+            </div>
+
+            <div class=" flex justify-center w-full sm:w-1/2 md:w-1/3 lg:w-1/6">
+                <img class="rounded-md w-60 h-40" src="https://enovathemes.com/propharm/wp-content/uploads/inst_placeholder_2.jpg" alt="Description de l'image" />
+            </div>
+
+            <div class="flex justify-center w-full sm:w-1/2 md:w-1/3 lg:w-1/6">
+                <img class="rounded-md w-60 h-40" src="https://enovathemes.com/propharm/wp-content/uploads/inst_placeholder_3.jpg" alt="Description de l'image" />
+            </div>
+
+            <div class=" flex justify-center w-full sm:w-1/2 md:w-1/3 lg:w-1/6">
+                <img class="rounded-md w-60 h-40" src="https://enovathemes.com/propharm/wp-content/uploads/inst_placeholder_4.jpg" alt="Description de l'image" />
+            </div>
+
+            <div class=" flex justify-center w-full sm:w-1/2 md:w-1/3 lg:w-1/6">
+                <img class="rounded-md w-60 h-40" src="https://enovathemes.com/propharm/wp-content/uploads/inst_placeholder_5.jpg" alt="Description de l'image" />
+            </div>
+
+            <div class="flex justify-center w-full sm:w-1/2 md:w-1/3 lg:w-1/6">
+                <img class="rounded-md w-60 h-40" src="https://enovathemes.com/propharm/wp-content/uploads/inst_placeholder_6.jpg" alt="Description de l'image" />
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+</html>
+<?php 
+include '../Footer/Footerview.php'
+?>
