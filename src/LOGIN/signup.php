b326d77e3804a918e008d5c477144e6de51a33c4
rrelma <elmahirachid24@gmail.com>
2024-05-27 22:08:08 +0100

    rachid

--- src/LOGIN/signup.php
+++ src/LOGIN/signup.php
@@ -42,6 +42,8 @@ session_start();
 <head>
 <script src="https://cdn.tailwindcss.com"></script>
 
+
+
     <link href="../output.css" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
