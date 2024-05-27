8875b0c267902e93375c1e53d7bdbf46b50b76b6
LabiadWeb <labiadmo920@gmail.com>
2024-05-27 20:46:45 +0100

    labias

--- src/NAVBAR/navbarcontroller.php
+++ src/NAVBAR/navbarcontroller.php
@@ -5,8 +5,11 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
   
-
+if(isset($_SESSION["panier"])) {
 $nbr = count ($_SESSION["panier"]);
+}else{
+    $nbr = 0;
+}
 include "navbarview.php";
 
 ?>
