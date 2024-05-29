d59dfab8c81326a00c3283ffdd2ca278f5b59ec2
LabiadWeb <labiadmo920@gmail.com>
2024-05-29 13:24:32 +0100

    labiad

---
+++ src/PDOModel.php
@@ -0,0 +1,11 @@
+<?php
+    include 'PDOModel.php';
+    class PDOModel{
+
+        public static function getconection(){
+            $conn = new PDO('mysql:host=localhost;port=3308;dbname=pharmacie', 'root', '');
+            return $conn;
+        }
+    }
+
+?>
