dee17d944090363a28c26a4d6f06ec3071cf821e
rrelma <elmahirachid24@gmail.com>
2024-05-31 14:53:05 +0100

    rachid

--- src/PDOModel.php
+++ src/PDOModel.php
@@ -3,7 +3,7 @@
     class PDOModel{
 
         public static function getconection(){
-            $conn = new PDO('mysql:host=localhost;port=3308;dbname=pharmacie', 'root', '');
+            $conn = new PDO('mysql:host=localhost;port=3306;dbname=pharmacie', 'root', '');
             return $conn;
           
     }
